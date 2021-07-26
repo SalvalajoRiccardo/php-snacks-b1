f87d6e6c7b0f9bb82b48a45c3312cfea0cb7eb9b
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-07-26 16:38:58 +0200

    fine snack 2

--- index.php
+++ index.php
@@ -27,6 +27,28 @@ $calendar = [
 $lunghezza = count($calendar);
 
 
+// Snack 2
+// Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
+if (empty($_GET['name']) || !isset($_GET['name']) || strlen($_GET['name']) < 4) {
+    $stringaName = 'errore';
+    var_dump($_GET['name']);
+} else {
+    $stringaName = $_GET['name'];
+};
+
+if (empty($_GET['mail']) || !isset($_GET['mail']) || !strpos($_GET['mail'], '@') || !strpos($_GET['mail'], '.')) {
+    $stringaMail = 'errore';
+} else {
+    $stringaMail = $_GET['mail'];
+};
+
+if (empty($_GET['eta']) || !isset($_GET['eta']) || !is_numeric($_GET['eta']) ) {
+    $stringaEta = 'errore';
+} else {
+    $stringaEta = $_GET['eta'];
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -37,10 +59,17 @@ $lunghezza = count($calendar);
     <title>Document</title>
 </head>
 <body>
+    <h2>snack 1</h2>
     <ul>
     <?php for ($i=0; $i < $lunghezza ; $i++) { ?>
-        <li><?php echo $calendar[$i]['nomeC'] . ' - ' . $calendar[$i]['nomeS'] ?> <?php echo $calendar[$i]['puntiC'] . ' | ' . $calendar[$i]['puntiS'] ?></li>
+        <li><?php echo $calendar[$i]['nomeC'] . ' - ' . $calendar[$i]['nomeS'] ?> <?php echo $calendar[$i]['puntiC'] . ' | ' . $calendar[$i]['puntiS']; ?></li>
     <?php } ?>
     </ul>
+
+    <h2>snack 2</h2>
+
+    <h3>Nome: <?php echo $stringaName; ?> </h3>
+    <h3>Mail: <?php echo $stringaMail; ?> </h3>
+    <h3>Età: <?php echo $stringaEta; ?> </h3>
 </body>
 </html>
